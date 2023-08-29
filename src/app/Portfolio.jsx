5223362c1540497a416bc9b00ae9ee0651d66bfb
Arka Prabha Chowdhury <arkopra@gmail.com>
2023-08-29 13:45:42 +0530

    image fixes

--- src/app/Portfolio.jsx
+++ src/app/Portfolio.jsx
@@ -22,10 +22,10 @@ const Portfolio = () => {
           <div class="flex flex-wrap -m-4">
             <div class="lg:w-1/3 sm:w-1/2 p-4">
               <div class="flex relative">
-                <Image
+                <img
                   alt="gallery"
                   class="absolute inset-0 w-full h-full object-center"
-                  src={work1}
+                  src="https://i.postimg.cc/QNynP0JY/work1.webp"
                 />
                 <a href="https://www.tremglobal.com" target='_blank'>
                   <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
