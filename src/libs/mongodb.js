9dad6ea99c38800866ad07e22aee7d1905571404
Arka Prabha Chowdhury <arkopra@gmail.com>
2023-09-17 18:49:02 +0530

    env change

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect("mongodb+srv://admin:gammainnovations@gammainnovations.6kp06s0.mongodb.net/?retryWrites=true&w=majority");
         console.log("MongoDB Connected");
     } catch (error) {
         console.log(error);
